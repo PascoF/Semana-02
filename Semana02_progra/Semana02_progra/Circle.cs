6dc3138ccfa314d2a1f598a83c452da27ec7f532
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-05 20:35:56 -0500

    .

---
+++ Semana02_progra/Semana02_progra/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02_progra
+{
+    class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(float radius)
+        {
+            this.name = "Circle";
+            this.radius = radius;
+        }
+
+        public override float CalculateArea()
+        {
+            return radius * radius * 3.14f;
+        }
+    }
+}
---
+++ Semana02_progra/Semana02_progra/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02_progra
+{
+    class Shape
+    {
+        protected string name;
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public virtual float CalculateArea()
+        {
+            return 0;
+        }
+    }
+}
